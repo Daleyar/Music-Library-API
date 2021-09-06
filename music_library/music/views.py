f235cd94c7cd73881cdaff5b7d8259977eb7cdfa
Ali Daley <ali.daley72@gmail.com>
2021-09-06 16:25:41 -0400

    added a class based view and added a method to get a song from its primary key

--- music_library/music/views.py
+++ music_library/music/views.py
@@ -4,6 +4,7 @@ from .serializers import SongSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
 
 
 # Create your views here.
@@ -18,4 +19,22 @@ class Songlist(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SongDetail(APIView):
+
+    def get_song(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        song = self.get_song(pk=pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        song = self.get_song(pk=pk)
+        serializer = SongSerializer(data=request.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
